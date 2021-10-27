935171c515faff991b0ad9660addb4d4faeb0813
tishamIslam <tisham.islam@gmail.com>
2021-10-27 11:27:51 -0400

    Ajouté détection d'erreur et amélioré code
    - si le type de compte n'est pas un valeur accepté, retourner un message d'erreur
    - ajouté des string resources à WelcomePage.java
    - implémenté des choses suggérés qui ont donné des avertissements dans Android Studio

--- App/app/src/main/java/com/example/novigrad/WelcomePage.java
+++ App/app/src/main/java/com/example/novigrad/WelcomePage.java
@@ -17,11 +17,10 @@ import com.google.firebase.database.ValueEventListener;
 
 public class WelcomePage extends AppCompatActivity {
 
-    private String username;
     private int accountType;
 
-    private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    private DatabaseReference myRef = database.getReference();
+    private final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private final DatabaseReference dbRef = database.getReference();
     private UserAccount user;
 
     @Override
@@ -30,19 +29,31 @@ public class WelcomePage extends AppCompatActivity {
         setContentView(R.layout.activity_welcome_page);
 
         Intent intent = getIntent();
-        username = intent.getStringExtra("userId");
+        String username = intent.getStringExtra("userId");
 
         TextView welcomeMessage = findViewById(R.id.welcomeMessage);
 
-        myRef.child("users").child(username).addValueEventListener(new ValueEventListener() {
+        dbRef.child("users").child(username).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot snapshot) {
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
                 user = snapshot.getValue(UserAccount.class);
                 String acctype;
-                if(user.getAccountType() == 0) acctype = "client";
-                else if(user.getAccountType() == 1) acctype = "employé";
-                else acctype = "administrateur";
-                welcomeMessage.setText("Bonjour " + user.getPrenom() +"!\n Bienvenue à Service Novigrad \n Vous êtes connecté en tant que " + acctype + ".");
+
+                assert user != null;
+                if(user.getAccountType() == 0) {
+                    acctype = "client";
+                }
+                else if (user.getAccountType() == 1) {
+                    acctype = "employé";
+                }
+                else if (user.getAccountType() == 2) {
+                    acctype = "administrateur";
+                } else {
+                    welcomeMessage.setText(R.string.badAccountTypeFoundErrorText);
+                    return;
+                }
+
+                welcomeMessage.setText(String.format(getString(R.string.loginSuccess), user.getPrenom(), acctype));
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {}
