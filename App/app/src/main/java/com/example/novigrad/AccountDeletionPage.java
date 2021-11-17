525705316098c16aebdc775b44806e6f0d98d86f
Celine Wan <mwanm074@uottawa.ca>
2021-11-17 00:12:17 -0500

    Ajout de bouton "retourn"
    Facilite la navigation

--- App/app/src/main/java/com/example/novigrad/AccountDeletionPage.java
+++ App/app/src/main/java/com/example/novigrad/AccountDeletionPage.java
@@ -103,4 +103,8 @@ public class AccountDeletionPage extends AppCompatActivity {
 
     }
 
+    public void onReturn(View view) {
+        finish();
+    }
+
 }
--- App/app/src/main/java/com/example/novigrad/ServicesPage.java
+++ App/app/src/main/java/com/example/novigrad/ServicesPage.java
@@ -85,4 +85,8 @@ public class ServicesPage extends AppCompatActivity {
 
     }
 
+    public void onReturn(View view){
+        finish();
+    }
+
 }
