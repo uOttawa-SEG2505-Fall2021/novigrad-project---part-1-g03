f02165911b178f5faddf7e4c222a717222f5ae9f
Tisham Islam <tisham.islam@gmail.com>
2021-12-04 17:23:52 -0500

    View demands from client side (#30)
    * added ViewClientDemandes and initial functionality
    
    * use several buttons to select which demands to display
    
    * added notification if demande approval status is changed
    
    * fixed merge conflict
    
    * Showing status of demandes + translation
    
    * added employee demande approval/rejection
    
    * Updated buttons
    
    Co-authored-by: Celine Wan <mwanm074@uottawa.ca>

--- App/app/src/main/java/com/example/novigrad/Demande.java
+++ App/app/src/main/java/com/example/novigrad/Demande.java
@@ -5,6 +5,12 @@ package com.example.novigrad;
  * */
 public class Demande {
 
+    //magic numbers for demande: 0 1 2
+    // 0 = pending
+    // 1 = approved
+    // 2 = rejected
+    private int status = 0;
+
     // instance variables
     private String firstName;
     private String lastName;
@@ -16,12 +22,13 @@ public class Demande {
     public Demande(){}
 
     // constructeur 1
-    public Demande(String firstName, String lastName, String nomDeUtilisateur, String nomDuServiceDemande, String nomSuccursaleDemande){
+    public Demande(String firstName, String lastName, String nomDeUtilisateur, String nomDuServiceDemande, String nomSuccursaleDemande, int status){
         this.firstName = firstName;
         this.lastName = lastName;
         this.nomDeUtilisateur = nomDeUtilisateur;
         this.nomDuServiceDemande = nomDuServiceDemande;
         this.nomSuccursaleDemande = nomSuccursaleDemande;
+        this.status = status;
     }
 
     // getters
@@ -45,8 +52,34 @@ public class Demande {
         return nomSuccursaleDemande;
     }
 
+    public int getStatus() {
+        return status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        // self check
+        if (this == o) {
+            return true;
+        }
+        // null check
+        if (o == null) {
+            return false;
+        }
+        // type check and cast
+        if (getClass() != o.getClass()) {
+            return false;
+        }
+        Demande demande = (Demande) o;
+        return demande.firstName.equals(this.firstName) &&
+                demande.lastName.equals(this.lastName) &&
+                demande.nomDeUtilisateur.equals(this.nomDeUtilisateur) &&
+                demande.nomDuServiceDemande.equals(this.nomDuServiceDemande) &&
+                demande.nomSuccursaleDemande.equals(this.nomSuccursaleDemande);
+    }
+
     @Override
     public String toString() {
-        return "Demande faite par " + firstName + " " + lastName + " pour le service \"" + nomDuServiceDemande +"\"";
+        return "Demande faite par " + firstName + " " + lastName + " à la succursale " + getNomSuccursaleDemande() + " pour le service \"" + nomDuServiceDemande +"\"";
     }
 }
--- App/app/src/main/java/com/example/novigrad/DemandeList.java
+++ App/app/src/main/java/com/example/novigrad/DemandeList.java
@@ -1,5 +1,6 @@
 package com.example.novigrad;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,12 +17,21 @@ public class DemandeList extends ArrayAdapter<Demande> {
     private final Activity context;
     List<Demande> demandes;
 
+    public int size() {
+        return demandes.size();
+    }
+
+    public List<Demande> getList() {
+        return demandes;
+    }
+
     public DemandeList(Activity context, List<Demande> demandes) {
         super(context, R.layout.layout_item_list, demandes);
         this.context = context;
         this.demandes = demandes;
     }
 
+    @SuppressLint("StringFormatMatches")
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
@@ -29,7 +39,16 @@ public class DemandeList extends ArrayAdapter<Demande> {
         TextView DemandeInfo = (TextView) listViewItem.findViewById(R.id.listItemName);
 
         Demande demande = demandes.get(position);
-        DemandeInfo.setText(String.format(context.getString(R.string.InfosSurDemande), demande.getFirstName(), demande.getLastName(), demande.getNomDeUtilisateur(), demande.getNomDuServiceDemande()));
+        int statusInt = demande.getStatus();
+        String demandeStatus;
+        if(statusInt==0){
+            demandeStatus = "En attente";
+        } else if (statusInt==1){
+            demandeStatus = "Approuvée";
+        } else {
+            demandeStatus = "Rejetée";
+        }
+        DemandeInfo.setText(String.format(context.getString(R.string.InfosSurDemande), demande.getFirstName(), demande.getLastName(), demande.getNomDeUtilisateur(), demande.getNomDuServiceDemande(), demande.getNomSuccursaleDemande(), demandeStatus));
         return listViewItem;
     }
 }
