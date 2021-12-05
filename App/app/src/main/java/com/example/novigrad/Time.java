00db9cf71df30a5ade03dc2e44d36d59e5e65e1f
emarth <emart133@uottawa.ca>
2021-12-05 17:20:28 -0500

    layout and classes

---
+++ App/app/src/main/java/com/example/novigrad/Time.java
@@ -0,0 +1,21 @@
+package com.example.novigrad;
+
+public class Time {
+
+    private int time;
+    private String day;
+
+    public Time(int times, String day) {
+        this.time = time;
+        this.day = day;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+}
---
+++ App/app/src/main/java/com/example/novigrad/TimeList.java
@@ -0,0 +1,35 @@
+package com.example.novigrad;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class TimeList extends ArrayAdapter<Time>{
+
+    private final Activity context;
+    List<Time> times;
+
+    public TimeList(Activity context, List<Time> times) {
+        super(context, R.layout.layout_time_list, times);
+        this.context = context;
+        this.times = times;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View timeListItem = inflater.inflate(R.layout.layout_time_list, null,true);
+        TextView dayLabel = timeListItem.findViewById(R.id.dayOfWeek);
+        TextView timeLabel = timeListItem.findViewById(R.id.timeOfDay);
+        Time time = times.get(position);
+        dayLabel.setText(time.getDay());
+        timeLabel.setText(Helpers.formatHHmm(time.getTime()));
+        return timeListItem;
+    }
+
+}
