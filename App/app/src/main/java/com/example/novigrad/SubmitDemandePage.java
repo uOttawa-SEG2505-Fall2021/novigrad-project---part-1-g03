7fdb4cb9f26f6f05d6ee3b1e5d5b98138bc9dac3
Celine Wan <mwanm074@uottawa.ca>
2021-12-08 18:30:31 -0500

    Added Return button + fix Toast Messages

--- App/app/src/main/java/com/example/novigrad/SubmitDemandePage.java
+++ App/app/src/main/java/com/example/novigrad/SubmitDemandePage.java
@@ -46,7 +46,7 @@ public class SubmitDemandePage extends AppCompatActivity {
 
                 if (!submitted) {
                     ratingBar.setRating(0);
-                    Toast.makeText(SubmitDemandePage.this, "Erreur: Vous n'avez pas encore soumit le demande", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SubmitDemandePage.this, "Erreur: Vous n'avez pas encore soumis la demande", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(SubmitDemandePage.this, "Service évalué à: " + rating, Toast.LENGTH_SHORT).show();
                     cote = rating;
@@ -114,19 +114,19 @@ public class SubmitDemandePage extends AppCompatActivity {
 
     public void onSubmitDemande(View view) {
         if (submitted) {
-            Toast.makeText(this, "Erreur: Vous avez déjà soumit le demande", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Erreur: Vous avez déjà soumis la demande", Toast.LENGTH_SHORT).show();
             return;
         }
         UserAccount user = UserAccount.getUserInstance();
         Demande newDemande = new Demande(user.getPrenom(), user.getNomDeFamille(), user.getNomDeUtilisateur(), applyServ.getNomService(), succName, 0);
         dbDemandes.push().setValue(newDemande);
-        Toast.makeText(this, "Votre demande a été soumise, SVP évaluez (optionnel)", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Votre demande a été soumise, SVP évaluez votre expérience (optionnel)", Toast.LENGTH_SHORT).show();
         submitted = true;
     }
 
     public void onSubmitRating(View view) {
         if (submitted) {
-            Toast.makeText(this, "Votre évaluation a été soumit.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Votre évaluation a été soumise.", Toast.LENGTH_SHORT).show();
             FirebaseDatabase.getInstance().getReference("ratings").push().setValue(new Rating(cote, succName)); //ratings --> ratingKey --> rating
             finish();
         } else {
